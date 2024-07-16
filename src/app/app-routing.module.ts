95b0a3b8b0dd52f04137fc3ee86cf295ad1c157d
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-07-16 19:12:16 -0300

    fix: refatoração das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavComponent } from './components/nav/nav.component';
+import { DespesasComponent } from './pages/despesas/despesas.component';
+import { RendimentosComponent } from './pages/rendimentos/rendimentos.component';
 
 const routes: Routes = [
   {
@@ -12,7 +14,20 @@ const routes: Routes = [
   },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'home',
+    component: NavComponent,
+    children: [
+      {
+        path: 'despesas',
+        component: DespesasComponent,
+      },
+      {
+        path: 'rendimentos',
+        component: RendimentosComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
