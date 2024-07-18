8b4c87a399c254a2da8e9290c27ef772819c3081
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-07-18 17:20:56 -0300

    fix: refatoração e organização dos imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,51 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { environment } from './environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
+import { DespesasComponent } from './pages/despesas/despesas.component';
+import { RendimentosComponent } from './pages/rendimentos/rendimentos.component';
+import { DashComponent } from './components/dash/dash.component';
+import { CardComponent } from './components/card/card.component';
+import { FormComponent } from './components/form/form.component';
+
+//Material
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DespesasComponent } from './pages/despesas/despesas.component';
-import { RendimentosComponent } from './pages/rendimentos/rendimentos.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+//firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from './environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent, NavComponent, DespesasComponent, RendimentosComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavComponent,
+    DespesasComponent,
+    RendimentosComponent,
+    DashComponent,
+    CardComponent,
+    FormComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -33,8 +59,15 @@ import { RendimentosComponent } from './pages/rendimentos/rendimentos.component'
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
-  providers: [],
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
