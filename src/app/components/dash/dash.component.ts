fe6bac97ace8f4c7d47984a5a9c62dea000ad6db
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-07-18 17:18:57 -0300

    feat: criação dash component

---
+++ src/app/components/dash/dash.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-dash',
+  templateUrl: './dash.component.html',
+  styleUrls: ['./dash.component.css'],
+})
+export class DashComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  // Observável que fornece o layout dinâmico com base no tamanho da tela
+  cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        // Layout para telas de mão (Handset)
+        return {
+          columns: 2,
+          chart: { cols: 1, rows: 2 },
+          table: { cols: 1, rows: 4 },
+        };
+      } else {
+        // Layout para telas maiores (desktop)
+        return {
+          columns: 2,
+          chart: { cols: 1, rows: 2 },
+          table: { cols: 2, rows: 3 },
+        };
+      }
+    })
+  );
+}
