51237e1a84f42a992d56564df2cb9d49b3fe0851
Cahmarchi95 <cah.de.marchi@hotmail.com>
2024-08-07 13:58:55 -0300

    feat: criação formulário específico despesa

---
+++ src/app/components/despesa-form/despesa-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DespesaService } from './../../services/despesa.service';
+import { catchError, of } from 'rxjs';
+
+@Component({
+  selector: 'app-despesa-form',
+  templateUrl: './despesa-form.component.html',
+  styleUrls: ['./despesa-form.component.css'],
+})
+export class DespesaFormComponent implements OnInit {
+  despesaForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private despesaService: DespesaService
+  ) {}
+
+  ngOnInit() {
+    this.despesaForm = this.fb.group({
+      despesa: ['', Validators.required],
+      valor: ['', Validators.required],
+      data: ['', Validators.required],
+    });
+  }
+
+  submit() {
+    if (this.despesaForm.valid) {
+      const despesa = {
+        despesa: this.despesaForm.value.despesa,
+        valor: this.despesaForm.value.valor,
+        data: this.despesaForm.value.data,
+      };
+
+      this.despesaService
+        .addDespesa(despesa)
+        .pipe(
+          catchError((error) => {
+            console.error('Erro ao cadastrar despesa:', error);
+            return of(undefined); // Retorna um Observable vazio em caso de erro
+          })
+        )
+        .subscribe({
+          next: () => {
+            alert('Despesa cadastrada com sucesso!');
+            this.despesaForm.reset();
+            this.clearValidators();
+          },
+          error: (error) => {
+            console.error('Erro ao cadastrar despesa:', error);
+          },
+        });
+    } else {
+      console.error('Formulário inválido. Verifique os campos.');
+    }
+  }
+
+  private clearValidators() {
+    this.despesaForm.get('despesa')?.clearValidators();
+    this.despesaForm.get('despesa')?.updateValueAndValidity();
+    this.despesaForm.get('valor')?.clearValidators();
+    this.despesaForm.get('valor')?.updateValueAndValidity();
+    this.despesaForm.get('data')?.clearValidators();
+    this.despesaForm.get('data')?.updateValueAndValidity();
+  }
+}
